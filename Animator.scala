ccb7c2bc7a6facbd42446d7a27ece58e2b7294b4
Dennis Albrecht <5196458+d-albrecht@users.noreply.github.com>
2021-12-30 17:32:33 +0100

    Update v2.5
    updated code and spreadsheets to reflect version 2.5
    added new sample-animations

--- Animator.scala
+++ Animator.scala
@@ -1,84 +1,91 @@
-import java.io.PrintWriter
-import scala.io.Source
+import scala.io.Source.fromFile
 
 object Animator extends App {
-	def param(n: Int, fb: String): String = {
-		args.toList.drop(n).headOption.getOrElse(fb)
-	}
+	def narg(a: String, fb: Int): Int = args.toList.filter(_.startsWith(s"-$a=")).map(_.substring(3).toInt).headOption.getOrElse(fb)
+	def targ(a: String, fb: String): String = args.toList.filter(_.startsWith(s"-$a=")).map(_.substring(3)).headOption.getOrElse(fb)
+	def flag(f: String, fb: Boolean): Boolean = args.contains(s"-$f") ^ fb
 
-	// parameters
-	val seedingFrames = 150
-	val seedingInterval = 12
-	val expansionFrames = 20
-	val fadingFrames = 180
-	val expansionRadius = 0.5
+	/* parameters */
+	val seedingFrames = narg("l", 150)
+	val seedingInterval = narg("i", 12)
+	val expansionFrames = narg("e", 20)
+	val fadingFrames = narg("f", 180)
+	val expansionRadius = narg("r", 5) / 10.0
+	val uniqueSeeds = !flag("b", false)
+	val flicker = !flag("c", false)
 
-	// file input
-	val distsRaw = Source.fromFile(param(0, "dists.csv")).getLines.toList.filter(_.size > 1)
-	val dists = distsRaw.drop(1).map(_.split(",").drop(1).map(_.toFloat)).map(_.toList)
-	val seedsRaw = Source.fromFile(param(1, "seeds.csv")).getLines.toList.filter(_.size > 1)
-	var seeds = seedsRaw.drop(1).map(_.split(",").map(_.toInt)).map(_.toList).map(s => s.updated(0, s(0) - 1))
+	/* file input */
+	val distsRaw = fromFile(targ("d", "dists.csv")).getLines.filter(_.size > 1).toList
+	val dists = distsRaw.drop(1).map(_.split(",").map(_.toFloat).toList)
+	val seedsRaw = fromFile(targ("s", "seeds.csv")).getLines.filter(_.size > 1).toList
+	var seeds = seedsRaw.drop(1).map(_.split(",").map(_.toInt).toList)
 	println("file input parsed")
 
-	// output file
-	val output = new PrintWriter(param(2, "explosions.csv"))
-	output.print("frame")
-	dists.zipWithIndex.map(_._2).foreach(i => output.write(s",r_$i,g_$i,b_$i"))
+	/* output file */
+	val output = new java.io.PrintWriter(targ("o", "explosions.csv"))
+	output.print("FRAME") // intentional not "FRAME_ID" to prevent sorting of already ordered rows
+	dists.zipWithIndex.map(_._2).foreach(i => output.write(s",R_$i,G_$i,B_$i"))
 	output.println()
 	println("output file prepared")
 
-	// prepend last few seeds to the beginning
-	val times = seeds.map(_(1)).sorted
-	val gaps = times.zip(times.tail :+ (times.head + seedingFrames))
-	val maxGap = gaps.map(g => (g._1, g._2, g._2 - g._1)).maxBy(_._3)
-	val newStart = maxGap._2
-	seeds = seeds.map(s => s.updated(1, (s(1) + seedingFrames - newStart) % seedingFrames))
-	println("shifted start to frame " + newStart)
+	/* prepend last few seeds to the beginning */
+	def rebase(seeds: List[List[Int]]): List[List[Int]] = {
+		val timings = seeds.map(_(1)).sorted
+		val gaps = timings.zip(timings.tail :+ (timings.head + seedingFrames))
+		val newStart = gaps.map(g => (g._1, g._2, g._2 - g._1)).reverse.maxBy(_._3)._2 % seedingFrames
+		println("shifting start to frame " + newStart)
+		seeds.map(s => s.updated(1, (s(1) + seedingFrames - newStart) % seedingFrames))
+	}
+	@scala.annotation.tailrec def uniquify(seeds: List[List[Int]]): List[List[Int]] = {
+		if (seeds.map(_(1)).distinct.size == seeds.size)
+			seeds
+		else {
+			val shifted = seeds.sortBy(_(1)).foldLeft((-1, 0, List[List[Int]]())){
+				case ((c, i, r), s) if (c < s(1)) => (s(1), i, s.updated(1, s(1) + i) :: r)
+				case ((c, i, r), s) if (c == s(1)) => (c, i + 1, s.updated(1, c + i + 1) :: r)
+			}
+			uniquify(rebase(shifted._3))
+		}
+	}
+	seeds = if(uniqueSeeds && seeds.size < seedingFrames / 2) uniquify(rebase(seeds)) else rebase(seeds)
 	val loopingFrames = seedingFrames - (expansionFrames + fadingFrames) / seedingInterval - 2
 	seeds = seeds ++ seeds.filter(_(1) > loopingFrames).map(s => s.updated(1, s(1) - seedingFrames))
 	println("infinite looping prepared. number of seeds now: " + seeds.size)
 
-	// generatin the actual color-table
+	/* generating the actual color-table */
 	def filterSeeds(frame: Int, d: List[Float])(s: List[Int]): Boolean = {
-		// expansion phase		
-		s(1) * seedingInterval <= frame &&
+		s(1) * seedingInterval <= frame && // expansion phase		
 		frame <= s(1) * seedingInterval + expansionFrames &&
 		d(s(0)) <= expansionRadius * (frame - s(1) * seedingInterval) / expansionFrames ||
-		// fading phase
-		s(1) * seedingInterval + expansionFrames <= frame &&
+		s(1) * seedingInterval + expansionFrames <= frame && // fading phase
 		frame <= s(1) * seedingInterval + expansionFrames + fadingFrames &&
 		d(s(0)) <= expansionRadius
 	}
-	// this helper function will corrupt both the index and the time frame information, don't rely on them anymore
-	def dimmPattern(frame: Int)(s: List[Int]): List[Int] = {
+	def dimmPattern(frame: Int)(s: List[Int]): List[Int] = { // corrupts index and the time frame information
 		if (frame <= s(1) * seedingInterval + expansionFrames) {
 			s
 		} else {
-			val flicker = scala.util.Random.nextFloat() * 0.125
-			val dimming = 1.0 * (s(1) * seedingInterval + expansionFrames + fadingFrames - frame) / fadingFrames + flicker
+			val flickering = scala.util.Random.nextFloat() * 0.125 * (if(flicker) 1 else 0)
+			val dimming = 1.0 * (s(1) * seedingInterval + expansionFrames + fadingFrames - frame) / fadingFrames + flickering
 			s.map(c => (c * dimming).toInt)
 		}
 	}
 	var frame = 0
 	for (seed <- 0 until seedingFrames) {
-		println("generating seeding frame " + seed)
+		output.flush() // don't keep too much in memory
 		for (subframe <- 0 until seedingInterval) {
 			output.print(frame)
 			dists.foreach(d => {
-				val patterns = seeds.filter(filterSeeds(frame, d))
-				val dimmed = patterns.map(dimmPattern(frame))
-				val r = dimmed.map(_(2)).sum
-				val g = dimmed.map(_(3)).sum
-				val b = dimmed.map(_(4)).sum
-				output.print(s",${Math.min(255,r)},${Math.min(255,g)},${Math.min(255,b)}")
+				val patterns = List(0, 0, 0, 0, 0) :: seeds.filter(filterSeeds(frame, d)).map(dimmPattern(frame))
+				output.print(patterns.transpose.drop(2).map(c => Math.min(255, c.sum)).mkString(",", ",", ""))
 			})
-			frame += 1
 			output.println()
+			frame += 1
 		}
 	}
 	println("color-table generated. frames generated: " + frame)
 
-	// flush and close output
+	/* finalize output */
 	output.flush()
 	output.close()
 	println("animator is done")
